ba5c762fba77829b33211e94b6387ad09b74b943
Ashwiniharode0 <115207004+Ashwiniharode0@users.noreply.github.com>
2024-04-15 10:25:14 +0530

    Update DoctorRegistry.sol

--- DoctorRegistry.sol
+++ DoctorRegistry.sol
@@ -1,1 +1,13 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.2;
+contract DoctorRegistry {
+    mapping(address => bool) private doctors;
+    event DoctorAdded(address doctor);
+    function addDoctor(address _doctor) public {
+        doctors[_doctor] = true;
+        emit DoctorAdded(_doctor);
+    }
+    function isDoctor(address _doctor) public view returns (bool) {
+        return doctors[_doctor];
+}
+}
